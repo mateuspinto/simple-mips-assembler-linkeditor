baa043b5ceed6d0ff24881fb788cbac65df5af02
Mateus P. Silva <mateus.p.silva@ufv.br>
2019-04-30 18:54:09 -0300

    finalizado linkeditor

--- exemplo.asm
+++ exemplo.asm
@@ -1,2 +1,5 @@
-AA:sllv $s1, $zero, $s2
-sll $s1, $zero, 0B01
+add $s2, $s0, $s1
+sll $t1, $s2, 2 
+or $t2, $s2, $s1
+andi $t2, $t1, 16
+addi $t3, $t2, -243
--- mipsMounter.py
+++ mipsMounter.py
@@ -3,10 +3,10 @@ import sys
 class mipsMounter(object):
     '''A Mounter for MIPS who turn assembly code into binary code'''
 
-    def __init__(self, inputFilename, outputFilename):
+    def __init__(self, inputFilename, outputFilename, wordSize = 4, startAdress=0):
         self.inputFilename = inputFilename
         self.outputFilename = outputFilename
-        self.labels = []
+        self.labels = {}
         self.registers = {"$zero":"00000",
                             "$s0":"10000",
                             "$s1":"10001",
@@ -23,11 +23,33 @@ class mipsMounter(object):
                             "$t4":"01100",
                             "$t5":"01101",
                             "$t6":"01110",
-                            "$t7":"01111"}
+                            "$t7":"01111",
+                            "$v0":"00010",
+                            "$v1":"00011",
+                            "$a0":"00100",
+                            "$a1":"00101",
+                            "$a2":"00110",
+                            "$a3":"00111",
+                            "$at":"00001",
+                            "$k0":"11010",
+                            "$k1":"11011",
+                            "$gp":"11100",
+                            "$sp":"11101",
+                            "$fp":"11110",
+                            "$ra":"11111"}
         self.mounted = []
+        self.linkEdited = []
+        self.wordSize = wordSize
+        self.startAdress = startAdress
         self.instructions = {"add":{
             "type":"r",
             "inputs":["rd", "rs", "rt"],
+            "fn":"100000"
+        },
+
+        "move":{
+            "type":"r",
+            "inputs":["rs", "rd"],
             "fn":"100000"
         },
         
@@ -179,10 +201,164 @@ class mipsMounter(object):
         "xor":{
             "type":"r",
             "inputs":["rd", "rs", "rt"],
-            "fn":"001100"
+            "fn":"100110"
+        },
+
+        "addi":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"001000"
+        },
+
+        "addiu":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"001001"
+        },
+
+        "andi":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"001100"
+        },
+
+        "beq":{
+            "type":"i",
+            "inputs":["rs", "rt", "label"],
+            "opcode":"000100"
+        },
+
+        "bgez":{
+            "type":"i",
+            "inputs":["e_rt-00001", "rs", "imm"],
+            "opcode":"000001"
+        },
+
+        "bgtz":{
+            "type":"i",
+            "inputs":["e_rt-00000", "rs", "imm"],
+            "opcode":"000111"
+        },
+
+        "blez":{
+            "type":"i",
+            "inputs":["e_rt-00000", "rs", "imm"],
+            "opcode":"000110"
+        },
+
+        "bltz":{
+            "type":"i",
+            "inputs":["e_rt-00000", "rs", "imm"],
+            "opcode":"000001"
+        },
+
+        "bne":{
+            "type":"i",
+            "inputs":["rs", "rt", "label"],
+            "opcode":"000101"
+        },
+
+        "lb":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"100000"
+        },
+
+        "lbu":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"100100"
+        },
+
+        "lh":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"100001"
+        },
+
+        "lb":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"100101"
+        },
+
+        "lui":{
+            "type":"i",
+            "inputs":["rt", "imm"],
+            "opcode":"001111"
+        },
+
+        "lw":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"100011"
+        },
+
+        "lwc1":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"110001"
+        },
+
+        "ori":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"001101"
+        },
+
+        "sb":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"101000"
+        },
+
+        "slti":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"001010"
+        },
+
+        "sltiu":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"001011"
+        },
+
+        "sh":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"101001"
+        },
+
+        "sw":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"101011"
+        },
+
+        "swc1":{
+            "type":"i",
+            "inputs":["rt", "imm(rs)"],
+            "opcode":"111001"
+        },
+
+        "xori":{
+            "type":"i",
+            "inputs":["rt", "rs", "imm"],
+            "opcode":"110001"
+        },
+
+        "j":{
+            "type":"j",
+            "inputs":["label"],
+            "opcode":"000010"
+        },
+
+        "jal":{
+            "type":"j",
+            "inputs":["label"],
+            "opcode":"000011"
         }
-        
-        
         }
 
     def printFilenames(self):
@@ -227,91 +403,189 @@ class mipsMounter(object):
         if str(n).replace("-", "").isnumeric():
             return mipsMounter.__NumToTc(int(n), bits) #The general case deals with decimal numbers
 
-    def __insertLabelReturnBinary(self, newLabel):
+    def __labelReturnIndex(self, label):
         '''Try to find the index of an assembly label. If it not exists, create a new one'''
-        try:
-            return mipsMounter.__intToBinary(self.labels.index(newLabel), 16)
-        except:
-            self.labels.append(newLabel)
-            return mipsMounter.__intToBinary(self.labels.index(newLabel), 16)
+        if label in self.labels:
+            return self.labels[label]
+
+    def __labelInsertLabel(self, newLabel, adress):
+        if newLabel in self.labels:
+            print("ERROR. TWO REFFERENCES TO " + newLabel)
+            sys.exit()
+        else:
+            self.labels[newLabel] = adress
 
     @staticmethod
     def __lineError(line):
         print("PARAMETER ERROR ON " + line)
         sys.exit()
 
+    def printMounted(self):
+        for line in self.mounted:
+            print(line)
+
+    def printLinkEdited(self):
+        for line in self.linkEdited:
+            print(line)
+
+    def saveMounted(self):
+        with open(self.outputFilename, "w") as output:
+            for line in self.mounted:
+                output.write(line + "\n")
+
+    def saveLinkEdited(self):
+        with open(self.outputFilename, "w") as output:
+            for line in self.linkEdited:
+                output.write(line + "\n")
+
     def mount(self):
         '''Turn MIPS assembly code into binary'''
 
-        self.labels=[]
-
         with open(self.inputFilename, "r") as input:
-            with open(self.outputFilename, "w") as output:
-                for line in input:
+            for line in input:
 
-                    if (line.startswith("#") or line.isspace()): #Ignore fullline comments or blank lines
-                        pass
+                if (line.startswith("#") or line.isspace()): #Ignore fullline comments or blank lines
+                    pass
 
-                    else: # Flow goes here when the line is a MIPS command
+                else: # Flow goes here when the line is a MIPS command
 
-                        line = (line.split("#",1))[0] #ignore inline comments
+                    line = (line.split("#",1))[0] #ignore inline comments
 
-                        swap = line.split(":")
-                        if len(swap) == 2:
-                            label = swap[0].strip() + ":"
-                            swap = swap[1]
-                        elif len(swap) == 1:
-                            label = ""
-                            swap = swap[0]
-                        else:
-                            self._mipsMounter__lineError
+                    swap = line.split(":")
+                    if len(swap) == 2:
+                        label = swap[0].strip() + ":"
+                        swap = swap[1]
+                    elif len(swap) == 1:
+                        label = ""
+                        swap = swap[0]
+                    else:
+                        self._mipsMounter__lineError
 
-                        swap = swap.split(" ",1)
-                        instruction = swap[0].lower().strip()
-                        parameters = swap[1].split(",")
+                    while swap.startswith(" "):
+                        swap = swap[1:] 
 
-                        # remove all parameters spaces Ex: "  $s0" turns into "$s0"
-                        for i in range(len(parameters)):
-                            parameters[i] = parameters[i].strip()
-                        
-                        if(instruction in self.instructions):
-                            self.mounted.append(label)
+                    swap = swap.split(" ",1)
+                    instruction = swap[0].lower().strip()
+                    parameters = swap[1].split(",")
 
-                            if(self.instructions[instruction]["type"] == "r"):
+                    # remove all parameters spaces Ex: "  $s0" turns into "$s0"
+                    for i in range(len(parameters)):
+                        parameters[i] = parameters[i].strip()
+                    
+                    if(instruction in self.instructions):
+                        self.mounted.append(label)
 
-                                opcode = "000000"
-                                rd = "00000"
-                                rs = "00000"
-                                rt = "00000"
-                                shamt = "00000"
-                                fn = "000000"
+                        if(self.instructions[instruction]["type"] == "r"):
 
-                                if(len(self.instructions[instruction]["inputs"]) != len(parameters)):
-                                    self._mipsMounter__lineError(line)
-                                    
-                                for input_number, input_name in enumerate(self.instructions[instruction]["inputs"]):
+                            opcode = "000000"
+                            rd = "00000"
+                            rs = "00000"
+                            rt = "00000"
+                            shamt = "00000"
+                            fn = "000000"
 
-                                    if input_name == "rs":
-                                        rs = self.registers[parameters[input_number]]
-                                    elif input_name == "rd":
-                                        rd = self.registers[parameters[input_number]]
-                                    elif input_name == "rt":
-                                        rt = self.registers[parameters[input_number]]
-                                    elif input_name == "sa":
-                                        shamt = mipsMounter.__numToBinary(parameters[input_number], 5)
+                            if(len(self.instructions[instruction]["inputs"]) != len(parameters)):
+                                self._mipsMounter__lineError(line)
+                                
+                            for input_number, input_name in enumerate(self.instructions[instruction]["inputs"]):
 
-                                fn = self.instructions[instruction]["fn"]
+                                if input_name == "rs":
+                                    rs = self.registers[parameters[input_number]]
+                                elif input_name == "rd":
+                                    rd = self.registers[parameters[input_number]]
+                                elif input_name == "rt":
+                                    rt = self.registers[parameters[input_number]]
+                                elif input_name == "sa":
+                                    shamt = mipsMounter.__numToBinary(parameters[input_number], 5)
 
-                                self.mounted[len(self.mounted)-1] += (opcode + rd + rs + rt + shamt + fn)
+                            fn = self.instructions[instruction]["fn"]
 
-                            elif(self.instructions[instruction]["type"] == "i"):
-                                pass
+                            self.mounted[len(self.mounted)-1] += (opcode + rs + rt + rd + shamt + fn)
 
-                            elif(self.instructions[instruction]["type"] == "j"):
-                                pass
-                        
-                        else:
-                            self._mipsMounter__lineError(line)
+                        elif(self.instructions[instruction]["type"] == "i"):
+                            
+                            opcode = "000000"
+                            rs = "00000"
+                            rt = "00000"
+                            imm = "0000000000000000"
+
+                            if(len(self.instructions[instruction]["inputs"]) != len(parameters)):
+                                self._mipsMounter__lineError(line)
+
+                            for input_number, input_name in enumerate(self.instructions[instruction]["inputs"]):
+
+                                if input_name == "rt":
+                                    rt = self.registers[parameters[input_number]]
+                                elif input_name == "rs":
+                                    rs = self.registers[parameters[input_number]]
+                                elif input_name == "imm":
+                                    imm = mipsMounter.__numToBinary(parameters[input_number], 16)
+                                elif input_name == "label":
+                                    imm = "-" + parameters[input_number]
+                                elif input_name == "imm(rs)":
+                                    aux = parameters[input_number].split("(")
+                                    rs = self.registers[aux[1].replace(")","".strip())]
+                                    imm = mipsMounter.__numToBinary(aux[0].strip(), 16)
+                                elif input_name == "e_rt-00000":
+                                    rt = "00000"
+                                elif input_name == "e_rt-00001":
+                                    rt = "00001"
+
+                            opcode = self.instructions[instruction]["opcode"]
+
+                            self.mounted[len(self.mounted)-1] += (opcode + rs + rt + imm)
+                            
+
+                        elif(self.instructions[instruction]["type"] == "j"):
+
+                            opcode = "000000"
+                            j = "0000000000000000000000000"
+
+                            if(len(self.instructions[instruction]["inputs"]) != len(parameters)):
+                                self._mipsMounter__lineError(line)
+
+                            for input_number, input_name in enumerate(self.instructions[instruction]["inputs"]):
+                                if input_name == "label":
+                                    j = "=" + parameters[input_number].strip()
+
+                            opcode = self.instructions[instruction]["opcode"]
+
+                            self.mounted[len(self.mounted)-1] += (opcode + j)
+                    
+                    else:
+                        self._mipsMounter__lineError(line)
+    def linkEdit(self):
+        pc = self.startAdress
+
+        for line_number, line in enumerate(self.mounted):
+            if ":" in line:
+                aux = line.split(":")
+                self.mounted[line_number] = aux[1]
+                self.__labelInsertLabel(aux[0].strip(), pc)
+            pc += self.wordSize
+
+        pc = self.startAdress
+        for line in self.mounted:
+            pc += self.wordSize
+
+            if "=" in line:
+
+                aux = line.split("=")
+                opcode = aux[0]
+                label = aux[1]
+                label = mipsMounter.__numToBinary(self._mipsMounter__labelReturnIndex(label), 26)
+                self.linkEdited.append(opcode + label)
+
+            elif "-" in line:
+
+                aux = line.split("-")
+                opcode = aux[0]
+                label = aux[1]
+                label = mipsMounter.__numToBinary(self._mipsMounter__labelReturnIndex(label) - pc, 16)
+                self.linkEdited.append(opcode + label)
+
+            else:
+                self.linkEdited.append(line)
 
 # if __name__ == '__main__':
 #     try:
@@ -323,3 +597,5 @@ class mipsMounter(object):
 
 mounter = mipsMounter("exemplo.asm", "exemplo.bin")
 mounter.mount()
+mounter.linkEdit()
+mounter.saveLinkEdited()
